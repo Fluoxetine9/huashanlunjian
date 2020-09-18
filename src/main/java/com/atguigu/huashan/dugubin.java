7304c5d44af366c1e5b0bc61448954ecbe90b0a3
xiaoliu <929487691@qq.com>
2020-09-18 18:55:27 +0800

    决战华山之巅！

--- src/main/java/com/atguigu/huashan/dugubin.java
+++ src/main/java/com/atguigu/huashan/dugubin.java
@@ -6,5 +6,6 @@ public class dugubin {
         System.out.println("广招天下勇士于月圆之夜华山论剑！");
         System.out.println("报名费300大洋！");
         System.out.println("性格懦弱者大可不必来！");
+        System.out.println("我用小号来参赛");
     }
 }
